056f718e3e5f4771e15ea4319839b785bdcffe29
hamza <hamza.ouggadi@gmail.com>
2025-06-28 00:00:50 +0100

    //

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
