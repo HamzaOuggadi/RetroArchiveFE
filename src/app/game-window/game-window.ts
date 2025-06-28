c6ea22a4f65ef20e388a7a7547ad906bfb35310c
hamza <hamza.ouggadi@gmail.com>
2025-06-28 22:23:51 +0100

    //

--- src/app/game-window/game-window.ts
+++ src/app/game-window/game-window.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-game-window',
@@ -6,6 +6,20 @@ import { Component } from '@angular/core';
   templateUrl: './game-window.html',
   styleUrl: './game-window.css'
 })
-export class GameWindow {
+export class GameWindow implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const ruffle = (window as any).RufflePlayer?.newest() ?? (window as any).RufflePlayer?.createPlayer();
+    const container = document.getElementById("game-container");
+
+    if (container && ruffle) {
+      container.innerHTML = '';
+      const player = ruffle.createPlayer();
+      player.style.width = "1000px";
+      player.style.height = "750px";
+      container.appendChild(player);
+      player.load("/games/Bloons.swf"); // replace with dynamic path if needed
+    }
+  }
 
 }
