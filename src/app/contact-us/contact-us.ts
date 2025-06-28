a745b870858eb87d921c8ece4a9e1712df0f521f
hamza <hamza.ouggadi@gmail.com>
2025-06-28 20:04:49 +0100

    //

--- src/app/contact-us/contact-us.ts
+++ src/app/contact-us/contact-us.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './contact-us.html',
   styleUrl: './contact-us.css'
 })
--- src/app/main-layout/main-layout.ts
+++ src/app/main-layout/main-layout.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {Sidebar} from './sidebar/sidebar';
 import {RouterOutlet} from '@angular/router';
+import {ContactUs} from '../contact-us/contact-us';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     Sidebar,
-    RouterOutlet
+    RouterOutlet,
+    ContactUs
   ],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
