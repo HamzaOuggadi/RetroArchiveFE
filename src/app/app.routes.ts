049e41d67d5cde01c4cf9ec59085dee11e11129c
hamza <hamza.ouggadi@gmail.com>
2025-06-28 14:43:48 +0100

    //

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import {ArchiveList} from './main-layout/archive-list/archive-list';
 
 export const routes: Routes = [
   {path : "home", component : Home},
+  {path : "", component : Home},
   {path : "archives", component : MainLayout, children : [
       {path : "", component : ArchiveList}, // variable with name of archives will be added later
       {path : "archive", component : Archive} // this will be used to display a specific archive, variable will be added later for the name of the archive
